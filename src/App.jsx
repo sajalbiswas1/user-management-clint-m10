802801aade205d2c48e5b491ee4b36006d13d9d0
Programing Hero <sajalb1899@gmail.com>
2023-10-13 18:28:17 +0600

    clint side

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,9 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [users, setUser] = useState([])
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target;
-    const name = form.name.value;
-    const email = form.email.value;
-    const result = {name,email}
-    console.log(result)
-  }
+function App() {
+  const [users, setUser] = useState([]);
 
   console.log(users)
   useEffect(() => {
@@ -20,6 +12,29 @@ function App() {
       .then(res => res.json())
       .then(data => setUser(data))
   }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = {name,email}
+    console.log(user)
+    fetch('http://localhost:5000/user', {
+      method: 'POST',
+      headers:{
+        'content-type':'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res=> res.json())
+    .then(data =>{
+      console.log(data);
+      const newUser = [...users, data];
+      setUser(newUser) ;
+    })
+  }
+
   return (
     <>
 
